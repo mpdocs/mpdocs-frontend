a5676b3891fa1925d89bbf5aaebb8e3f94bf55ca
dangannn <dn14072004@mail.ru>
2024-03-07 00:01:04 +0300

    feat: reports form initial

---
+++ src/app/components/ReportsForm/index.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import {useFieldArray, useForm} from "react-hook-form";
+
+type ReportsData = {
+    [key: string]: string[];
+};
+
+
+let exampleData: ReportsData = {
+    "1.	Информация о повышении квалификации в период 2021-22 уч. год": ["Форма повышения квалификации", "Страна. организация", "Наименование курса (дисциплины)", "№ диплома (свидетельства), дата выдачи", "Количество часов"],
+    "1.	Информация о повышении квалификации в период 2021-22 уч. ": ["Форма повышения квалификации", "Страна. организация", "Наименование курса (дисциплины)", "№ диплома (свидетельства), дата выдачи", "Количество часов"],
+}
+
+type FormValues = {
+    fieldArray: { name: string }[];
+};
+const ReportsForm = () => {
+    const {
+        register,
+        handleSubmit,
+        control,
+        watch,
+        reset,
+        formState,
+        setError,
+        formState: {isSubmitSuccessful, errors},
+
+    } = useForm<FormValues>();
+
+    const {fields, append, prepend, remove, swap, move, insert} = useFieldArray({
+        control,
+        name: "fieldArray",
+    });
+    const watchFieldArray = watch("fieldArray");
+    const controlledFields = fields.map((field, index) => {
+        return {
+            ...field,
+            ...watchFieldArray[index]
+        };
+    });
+    return (
+        <form>
+            {Object.keys(exampleData).map((key, fieldsetIndex) => (
+                <fieldset
+                    className="flex flex-col gap-2 border-2 border-white p-4"
+                    key={key}>
+                    <legend>{key}</legend>
+                    {exampleData[key].map((label, index) => (
+                        <label
+                            className="flex flex-col gap-2"
+                            htmlFor={`${fieldsetIndex}-${index}`} key={`${fieldsetIndex}-${index}`}>
+                            <span>{label}</span>
+                            <textarea id={`${fieldsetIndex}-${index}`}
+                                      {...register(`fieldArray.${fieldsetIndex * 100 + index}` as const, {
+                                          required: true,
+                                      })}
+                            />
+                        </label>
+                    ))}
+                </fieldset>
+            ))}
+            <button
+                type={"submit"}
+                disabled={!errors.root}
+                className="text-black bg-[#C2C2C2] rounded-3xl px-6 py-2 disabled:bg-amber-700"
+            >
+                Отправить
+            </button>
+        </form>
+    );
+};
+
+export default ReportsForm;
---
+++ src/app/reports/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+import React from 'react';
+import ReportsForm from "@/app/components/ReportsForm";
+
+const Page = () => {
+    return (
+        <div>
+            <section className="w-full h-screen flex justify-center items-center">
+                <ReportsForm/>
+            </section>
+        </div>
+    );
+};
+
+export default Page;
