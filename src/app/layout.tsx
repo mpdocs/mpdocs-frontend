ce71af3e37a02ca6141ef8172d2f098987b579f7
dangannn <dn14072004@mail.ru>
2024-03-08 15:58:16 +0300

    feat: providers component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,16 @@
-'use client'
 import "./globals.css";
 
 import {Inter} from "next/font/google";
-import {Provider} from "react-redux";
-
-import {store} from "@/utils/store";
+import {Providers} from "@/utils/components/Providers";
 
 const inter = Inter({subsets: ["latin"]});
 
-export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
-}>) {
+export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
     return (
         <html lang="en">
-        <Provider store={store}>
+        <Providers>
             <body className={inter.className}>{children}</body>
-        </Provider>
+        </Providers>
         </html>
     );
 }
