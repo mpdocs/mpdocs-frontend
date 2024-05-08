c739a4be67100f8fd177a7e811ccf6842e2f5a00
Брянцев Александр <bryancev.a2@wildberries.work>
2024-05-08 20:31:29 +0300

    feat: add dynamically theme switching

--- src/utils/components/Providers.tsx
+++ src/utils/components/Providers.tsx
@@ -1,19 +1,31 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { Provider } from "react-redux";
 import { store } from "@/utils/store";
-import { ConfigProvider, theme } from "antd";
+import { ConfigProvider, Switch, theme } from "antd";
+import { BulbFilled, CloudFilled } from "@ant-design/icons";
+import { Content, Header } from "antd/es/layout/layout";
 
 export function Providers({ children }: { children: ReactNode }) {
+  const { defaultAlgorithm, darkAlgorithm } = theme;
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const handleClick = () => {
+    setIsDarkMode((prevValue) => !prevValue);
+  };
+
   return (
     <Provider store={store}>
       <ConfigProvider
         theme={{
-          algorithm: theme.darkAlgorithm,
+          algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
         }}
       >
-        {children}
+        <Header>
+          <Switch checkedChildren={<BulbFilled />} unCheckedChildren={<CloudFilled />} onChange={handleClick} />
+        </Header>
+        <Content>{children}</Content>
       </ConfigProvider>
     </Provider>
   );
