f422e9547437b503bb30c23058e48626c15e734b
dangannn <dn14072004@mail.ru>
2024-05-10 16:21:44 +0400

    fix: theme

--- src/utils/components/Providers.tsx
+++ src/utils/components/Providers.tsx
@@ -1,30 +1,23 @@
 "use client";
 
-import { ReactNode, useState } from "react";
+import { ReactNode } from "react";
 import { Provider } from "react-redux";
 import { store } from "@/utils/store";
-import { ConfigProvider, Switch, theme } from "antd";
-import { BulbFilled, CloudFilled } from "@ant-design/icons";
-import { Content, Header } from "antd/es/layout/layout";
+import { ConfigProvider } from "antd";
+import { Content } from "antd/es/layout/layout";
 
 export function Providers({ children }: { children: ReactNode }) {
-  const { defaultAlgorithm, darkAlgorithm } = theme;
-  const [isDarkMode, setIsDarkMode] = useState(true);
-
-  const handleClick = () => {
-    setIsDarkMode((prevValue) => !prevValue);
-  };
-
   return (
     <Provider store={store}>
       <ConfigProvider
         theme={{
-          algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
+          token: {
+            colorPrimary: "#00b96b",
+            borderRadius: 2,
+            colorBgContainer: "#f6ffed",
+          },
         }}
       >
-        <Header>
-          <Switch checkedChildren={<BulbFilled />} unCheckedChildren={<CloudFilled />} onChange={handleClick} />
-        </Header>
         <Content>{children}</Content>
       </ConfigProvider>
     </Provider>
