e5e52c26aeeb4d3dcb6825c1a8f3877f539deaf0
dangannn <dn14072004@mail.ru>
2024-05-11 20:14:04 +0400

    fix: types for isAuth

--- src/app/auth/isAuth.tsx
+++ src/app/auth/isAuth.tsx
@@ -1,11 +1,11 @@
 "use client";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { redirect } from "next/navigation";
 import { useSelector } from "react-redux";
 import { RootState } from "@/utils/store";
 
-export default function isAuth(Component: any) {
-  return function IsAuth(props: any) {
+export default function isAuth<P extends Record<string, unknown>>(Component: React.ComponentType<P>): React.FC<P> {
+  return function IsAuth(props) {
     const user = useSelector((state: RootState) => state.auth.user);
 
     useEffect(() => {
