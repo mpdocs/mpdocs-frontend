11cde8301f4f320a90a3ddeda0d699c39978ee4c
dangannn <dn14072004@mail.ru>
2024-05-26 13:27:45 +0300

    feat: login button if unauth

--- src/app/components/Navbar/RightMenu.tsx
+++ src/app/components/Navbar/RightMenu.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Menu, MenuProps } from "antd";
+import { Button, Menu, MenuProps } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import { refreshUserData } from "@/utils/auth/thunk";
 import { useAppDispatch } from "@/utils/hooks/useAppDispatch";
@@ -8,26 +8,50 @@ import { RootState } from "@/utils/store";
 import Link from "next/link";
 import { IMenu } from "@/utils/types";
 import styles from "./index.module.scss";
+import { useRouter } from "next/navigation";
 
 const RightMenu: React.FC<IMenu> = ({ current, menu }) => {
   const dispatch = useAppDispatch();
   const user = useSelector((state: RootState) => state.auth.user);
-
+  const router = useRouter();
+  const moveToAuth = () => {
+    router.push("/auth");
+  };
   useEffect(() => {
     dispatch(refreshUserData());
   }, []);
-  const items: MenuProps["items"] = [
-    {
-      key: "profile",
-      label: (
-        <>
-          <Link href="/profile" className={styles.menu__username}>{`${user.first_name} ${user.last_name}`}</Link>
-          <UserOutlined />
-        </>
-      ),
-    },
-  ];
-  return <Menu onClick={menu.onClick} selectedKeys={[current]} mode={menu.mode} items={items} />;
+  let items: MenuProps["items"];
+  if (user.id) {
+    items = [
+      {
+        key: "profile",
+        label: (
+          <>
+            <Link href="/profile" className={styles.menu__username}>{`${user.first_name} ${user.last_name}`}</Link>
+            <UserOutlined />
+          </>
+        ),
+      },
+    ];
+  } else {
+    items = [
+      {
+        key: "login",
+        label: (
+          <>
+            <Button onClick={moveToAuth} type="primary">
+              Войти
+            </Button>
+          </>
+        ),
+      },
+    ];
+  }
+  return (
+    <>
+      <Menu onClick={menu.onClick} selectedKeys={[current]} mode={menu.mode} items={items} />
+    </>
+  );
 };
 
 export default RightMenu;
