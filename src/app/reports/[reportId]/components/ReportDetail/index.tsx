39d0d71b01360918345d72fe11e712f883f2aa80
Sergey Yaksanov <iaksanov2044@mail.ru>
2024-06-19 11:07:50 +0300

    feat: minor changes

--- src/app/reports/[reportId]/components/ReportDetail/index.tsx
+++ src/app/reports/[reportId]/components/ReportDetail/index.tsx
@@ -11,7 +11,12 @@ const ReportDetail: React.FC<ReportDetailProps> = ({ report }) => {
     <section className={styles.root}>
       <h2 className={styles.title}>{report.template.name}</h2>
       {/* TODO: edit form will be here */}
-      <p>{report.id}</p>
+      <p>Отчёт успешно создан!</p>
+      <p>
+        <a href={`${process.env.NEXT_PUBLIC_API_URL}/reports/${report.id}/generate`} target="_blank">
+          Посмотреть docx
+        </a>
+      </p>
     </section>
   );
 };
