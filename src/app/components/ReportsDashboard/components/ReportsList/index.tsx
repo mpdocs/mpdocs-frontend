a9bec07742c7bfe228be2c059e28efe9d2a89a72
Брянцев Александр <bryancev.a2@wildberries.work>
2024-06-27 19:11:32 +0300

    feat: translate fields && view edit btns

--- src/app/components/ReportsDashboard/components/ReportsList/index.tsx
+++ src/app/components/ReportsDashboard/components/ReportsList/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card } from "antd";
+import { Button, Card } from "antd";
 import { Report } from "../../types";
 import styles from "./index.module.scss";
 interface ReportsListProps {
@@ -16,31 +16,29 @@ const ReportsList: React.FC<ReportsListProps> = ({ sortedReports }) => {
     <div className={styles.wrapper}>
       {sortedReports.map((report, index) => (
         <Card className={styles.card} key={index} title={report.template.name}>
-          <div>
-            <h3>User</h3>
-            <p>
-              Name: {report.user.first_name} {report.user.last_name}, Username: {report.user.username}
-            </p>
-          </div>
-          <div>
-            <h3>Status</h3>
-            <p>
-              Is Reviewed:{" "}
-              {report.is_reviewed ? (
-                <span className={styles.statusTrue}>Yes</span>
-              ) : (
-                <span className={styles.statusFalse}>No</span>
-              )}
-            </p>
-          </div>
-          <div>
-            <h3>Timestamps</h3>
-            <p>
-              Created At: <span>{formatDate(report.created_at)}</span>
-            </p>
-            <p>
-              Updated At: <span>{formatDate(report.updated_at)}</span>
-            </p>
+          <p>
+            ФИО: {report.user.first_name} {report.user.last_name}
+          </p>
+          <p>Логин: {report.user.username}</p>
+          <p>
+            Проверено:{" "}
+            {report.is_reviewed ? (
+              <span className={styles.statusTrue}>Да</span>
+            ) : (
+              <span className={styles.statusFalse}>Нет</span>
+            )}
+          </p>
+          <p>
+            Время создания: <span>{formatDate(report.created_at)}</span>
+          </p>
+          <p>
+            Время обновления: <span>{formatDate(report.updated_at)}</span>
+          </p>
+          <div className={styles.buttons}>
+            <Button type="primary">Посмотреть</Button>
+            <Button type="primary" danger>
+              Редакитировать
+            </Button>
           </div>
         </Card>
       ))}
