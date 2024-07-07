fefe452499d8545f57fce3de6d15bd80b83c3e48
Брянцев Александр <bryancev.a2@wildberries.work>
2024-07-07 22:51:52 +0300

    fix: form handler fn

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -1,12 +1,31 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import isAuth from "@/app/auth/isAuth";
 import ReportForm from "@/app/components/ReportForm";
+import { SubmitHandler } from "react-hook-form";
+import { defaultValues, ReportFormValues } from "@/app/components/ReportForm/types";
+import api from "@/utils/api";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
+  const router = useRouter();
+  const [resetForm, setResetForm] = useState<() => void>(() => {});
+
+  const sendReport: SubmitHandler<ReportFormValues> = async (data) => {
+    api
+      .post("/reports/", data)
+      .then((resp) => {
+        resetForm();
+        router.push(`/reports/${resp.data.id}`);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
   return (
-    <section className="flex flex-col justify-center items-center">
-      <ReportForm />
+    <section>
+      <ReportForm initialData={defaultValues} onSubmit={sendReport} setResetForm={setResetForm} />
       {/*   FIXME: сюда положим список заполненных отчетов и форму заполнения нового если он еще не заполнен */}
     </section>
   );
