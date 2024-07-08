6ed5465402d9cb1cb6e9d763c5830d4aa294e324
dangannn <dn14072004@mail.ru>
2024-07-08 15:27:37 +0400

    feat: admin link

--- src/app/components/Navbar/LeftMenu.tsx
+++ src/app/components/Navbar/LeftMenu.tsx
@@ -1,29 +1,59 @@
 import React from "react";
-import { Menu } from "antd";
+import { Menu, MenuProps } from "antd";
 import Link from "next/link";
 import styles from "@/app/components/Navbar/index.module.scss";
 import { IMenu } from "@/utils/types";
-
-const items = [
-  {
-    key: "home",
-    label: (
-      <Link href={"/"}>
-        <span className={styles.header__logo}>Отчеты</span>
-      </Link>
-    ),
-  },
-  {
-    key: "create_report",
-    label: <Link href="/reports">Заполнить</Link>,
-  },
-  {
-    key: "report_list",
-    label: <Link href="/reports_list">Смотреть</Link>,
-  },
-];
+import { useSelector } from "react-redux";
+import { RootState } from "@/utils/store";
 
 const LeftMenu: React.FC<IMenu> = ({ current, menu }) => {
+  const user = useSelector((state: RootState) => state.auth.user);
+  const items: MenuProps["items"] = !user.is_staff
+    ? [
+        {
+          key: "home",
+          label: (
+            <Link href={"/"}>
+              <span className={styles.header__logo}>Отчеты</span>
+            </Link>
+          ),
+        },
+        {
+          key: "create_report",
+          label: <Link href="/reports">Заполнить</Link>,
+        },
+        {
+          key: "report_list",
+          label: <Link href="/reports_list">Смотреть</Link>,
+        },
+      ]
+    : [
+        {
+          key: "home",
+          label: (
+            <Link href={"/"}>
+              <span className={styles.header__logo}>Отчеты</span>
+            </Link>
+          ),
+        },
+        {
+          key: "create_report",
+          label: <Link href="/reports">Заполнить</Link>,
+        },
+        {
+          key: "report_list",
+          label: <Link href="/reports_list">Смотреть</Link>,
+        },
+        {
+          key: "download_stats",
+          label: (
+            <Link href={`${process.env.NEXT_PUBLIC_API_URL}/reports/stats/generate/`} target="_blank">
+              Скачать статистику
+            </Link>
+          ),
+        },
+      ];
+
   return <Menu onClick={menu.onClick} selectedKeys={[current]} mode={menu.mode} items={items} />;
 };
 
